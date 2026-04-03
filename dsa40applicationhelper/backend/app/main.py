ca744592027c648d3154a1c675293fc7abc646b3
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-03 10:26:19 +0200

    feat(backend): add health check

--- dsa40applicationhelper/backend/app/main.py
+++ dsa40applicationhelper/backend/app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from app.database import init_db
+from app.routers import health
 app = FastAPI(title="DSA40 Application Helper")
 
 init_db()
 
+app.include_router(health.router)
 @app.get("/")
 def index_fallback() -> JSONResponse:
     return JSONResponse({"message": "Hello World!"})
