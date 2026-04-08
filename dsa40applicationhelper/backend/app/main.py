cf28f9332e641d72c3f3d0c7f9f3b0cd6046bc60
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-08 15:24:59 +0200

    feat: export openapi definition from fastapi

--- dsa40applicationhelper/backend/app/main.py
+++ dsa40applicationhelper/backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.routing import APIRoute
 
 from app.database import init_db
 from app.routers import health
 from app.routers import vlopse
-app = FastAPI(title="DSA40 Application Helper")
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.path}-{route.name}"
+
+
+app = FastAPI(
+    title="DSA40 Application Helper",
+    generate_unique_id_function=custom_generate_unique_id,
+)
 
 init_db()
 
