a7f39c3143c7d29b2757119af3698e5af71a4bff
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-09 12:56:38 +0200

    feat(backend): add formservice to interact with core

--- dsa40applicationhelper/backend/app/routers/form.py
+++ dsa40applicationhelper/backend/app/routers/form.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
 from pydantic import BaseModel
 
-from app.core.mapping import QuestionMapper
 from app.core.models import Answer, MappedAnswer, MappingError
-from app.core.transform import AnswerTransformer
+from app.services.form_engine import FormService
 from app.services.vlopse import VlopseConfigService
 
 from .schemas.form import DSAQuestion
@@ -11,6 +10,7 @@ from .schemas.form import DSAQuestion
 router = APIRouter()
 
 service = VlopseConfigService()
+form_service = FormService()
 
 
 @router.get("/api/questions")
@@ -21,8 +21,7 @@ async def applicable_questions(vlopse: list[str] = Query(...)) -> list[DSAQuesti
     missing = [s for s in selected_vlopses if s not in vlopses]
     if len(missing):
         raise HTTPException(status_code=322, detail=f"Unknown vlopse(s): {missing}")
-    mapper = QuestionMapper.from_vlopse_names(vlopse)
-    qs = mapper.map()
+    qs = form_service.get_required_questions_for(vlopse)
 
     print(f"Returning {qs}")
 
@@ -44,13 +43,10 @@ class TransformResponse(BaseModel):
 
 @router.post("/api/transform")
 async def transform_answers(answers: AnswerRequest, vlopse: list[str] = Query(...)):
-    result = []
-    for klops in vlopse:
-        mapper = AnswerTransformer.from_vlopse_name(klops)
-        result.append(
-            TransformResponseForVlopse(name=klops, answers=mapper.map(answers.answers))
-        )
+    answers_inner = answers.answers
+    result = form_service.transform_answers_to_vlopse_answers(vlopse, answers_inner)
+    res = [TransformResponseForVlopse(name=name, answers=res) for name, res in result]
 
     print(f"Returning {result}")
-    response = TransformResponse(by_vlopse=result)
+    response = TransformResponse(by_vlopse=res)
     return response
--- dsa40applicationhelper/backend/app/services/form_engine.py
+++ dsa40applicationhelper/backend/app/services/form_engine.py
@@ -1,10 +1,24 @@
-from pathlib import Path
-
-from pydantic import TypeAdapter
-
-from app.models import UnifiedQuestion
-
-someadapter = TypeAdapter(list[UnifiedQuestion])
+from app.core.mapping import QuestionMapper
+from app.core.models import Answer, MappedAnswer, MappingError
+from app.core.transform import AnswerTransformer
 
 
-_DATA_DIR = Path(__file__).parent.parent / "data"
+class FormService:
+    def __init__(self) -> None:
+        pass
+
+    def get_required_questions_for(self, vlopses: list[str]):
+        # TODO: QuestionMapper should not load questions but receive them
+        mapper = QuestionMapper.from_vlopse_names(vlopses)
+        qs = mapper.map()
+        return qs
+
+    def transform_answers_to_vlopse_answers(
+        self, vlopses: list[str], answers: list[Answer]
+    ):
+        result: list[tuple[str, list[MappedAnswer | MappingError]]] = []
+        for klops in vlopses:
+            # TODO: AnswerTransformer should not load questions or mappings but receive them
+            transformer = AnswerTransformer.from_vlopse_name(klops)
+            result.append((klops, transformer.map(answers)))
+        return result
