8f40a70b7a859a6888a83b6da3ce5134f689894c
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-09 11:48:31 +0200

    refactor(backend): split question router

---
+++ dsa40applicationhelper/backend/app/routers/questions.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+
+from app.models import VLOPSEQuestion
+from app.schemas import InputTypeWithOptions
+from app.services.questions import QuestionService
+from app.services.vlopse import VlopseConfigService
+
+router = APIRouter()
+vlopse_service = VlopseConfigService()
+question_service = QuestionService(None)
+
+
+def to_pydantic(question: VLOPSEQuestion):
+    i_type = {"i_type": question.input_type}
+    i_type.update(question.options)
+    return VlopseQuestion(
+        id=question.id,
+        text=question.text,
+        required=question.required,
+        vlopse=question.vlopse,
+        input_type=i_type,
+    )
+
+
+class PostVlopseQuestionRequest(BaseModel):
+    id: str | None
+    text: str
+    required: bool
+    input_type: InputTypeWithOptions = Field(discriminator="i_type")
+
+
+@router.get("/api/vlopse/{name}/question")
+async def get_questions(name: str) -> JSONResponse:
+    questions = question_service.get_all_for_vlopse(vlopse=name)
+    result = []
+    for q in questions:
+        result.append(to_pydantic(q))
+
+    return result
+
+
+class VlopseQuestion(BaseModel):
+    id: str
+    text: str
+    required: bool
+    vlopse: str
+    input_type: InputTypeWithOptions = Field(discriminator="i_type")
+
+
+@router.get("/api/vlopse/{vlopse_name}/question/{question_id}")
+async def get_question(vlopse_name: str, question_id: str) -> JSONResponse:
+    question = question_service.get(question_id)
+    if question is None:
+        raise HTTPException(status_code=404, detail="question not found")
+
+    return to_pydantic(question)
+
+    res = PostVlopseQuestionRequest.model_validate(question)
+
+    return JSONResponse(res)
+
+
+@router.post("/api/vlopse/{name}/question")
+async def add_question(name: str, question: PostVlopseQuestionRequest) -> JSONResponse:
+    question_service.add(
+        vlopse=name,
+        **question.model_dump(exclude={"input_type"}),
+        input_type_with_options=question.input_type,
+    )
+
+    return JSONResponse({"success": True})
