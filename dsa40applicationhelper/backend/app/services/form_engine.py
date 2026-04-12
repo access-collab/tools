8e165f7b2293440335d3373430176f088bc73163
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-12 21:09:33 +0200

    feat(backend): add make iso

--- dsa40applicationhelper/backend/app/services/form_engine.py
+++ dsa40applicationhelper/backend/app/services/form_engine.py
@@ -1,3 +1,5 @@
+from pycountry import countries
+
 from app.core.mapping import QuestionMapper
 from app.core.models import Answer, MappedAnswer, MappingError
 from app.core.transform import AnswerTransformer
@@ -7,6 +9,13 @@ class FormService:
     def __init__(self) -> None:
         self.question_service = QuestionService()
         pass
+
+    def compute_options(self, question: DSAQuestion, vlopses: list[str]):
+        # TODO: think this should go to mapper and check dependent questions
+        if question.input_type == InputType.ISO_3166_1:
+            return [c.name for c in countries]
+        else:
+            return None
 
     def get_required_questions_for(self, vlopses: list[str]):
         # TODO: QuestionMapper should not load questions but receive them
