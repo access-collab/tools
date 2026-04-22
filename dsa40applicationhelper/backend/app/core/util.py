930c04367a5ec52d4c6b5a406a8cde5e236f46f6
glaserL <33624744+glaserL@users.noreply.github.com>
2026-04-22 12:10:24 +0200

    refactor(backend): simplify inputs search

--- dsa40applicationhelper/backend/app/core/util.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from typing import Any
-
-_DATA_DIR = Path(__file__).parent.parent / "data"
-
-
-def find_inputs_for_multiple(candidates: dict[str, Any], keys: list[str]):
-    print(f"Finding multiple {keys} in {candidates}")
-    return {k: v for k, v in candidates.items() if k in keys}
