1227d30ebf10e052778ec18a14eed54494daffbb
glaserL <33624744+glaserL@users.noreply.github.com>
2026-05-05 16:23:22 +0200

    feat(frontend): differentiate between input types
    feat(frontend): add input component via shadcn
    
    20e62b2

--- dsa40applicationhelper/frontend/src/lib/Question.svelte
+++ dsa40applicationhelper/frontend/src/lib/Question.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-  import type { InputType } from "@api";
+  import type { InputType, DsaQuestion } from "@api/types.gen";
   import Select from "$lib/Select.svelte";
+  import { Input } from "./components/ui/input";
 
   type Props = {
     id: string;
     text: string;
     input_type: InputType;
     help_text?: string | null;
+    config?: DsaQuestion["config"];
+    options?: string[] | null;
     value?: string;
     validation?: string | string[] | null;
   };
@@ -23,11 +26,27 @@
 
 <div>
   <label>Q ({id}): {text} <input name={id} {id} type="text" bind:value /></label>
+  <p class="font-medium">{text}</p>
   {#if help_text}
     {help_text}
   {/if}
   {#if validation}
     <p style:color="red">Err: {validation}</p>
+    {#if input_type === "text"}
+      <Input {id} name={id} type="text" bind:value class="max-w-xs" />
+    {:else if input_type === "file_upload"}
+      <Input {id} name={id} type="text" bind:value class="max-w-xs" />
+    {:else if input_type === "date_select"}
+      <Input {id} name={id} type="text" bind:value class="max-w-xs" />
+    {:else if input_type === "iso-3166-1"}
+      <Input
+        {id}
+        name={id}
+        type="text"
+        placeholder="Country name"
+        bind:value
+      />
+    {/if}
   {/if}
   <hr />
 </div>
