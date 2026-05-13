7028f227916a0dad2880d55898f4498dbf47ce4d
glaserL <33624744+glaserL@users.noreply.github.com>
2026-05-13 11:59:18 +0200

    test(backend): adjust vlopse tests

--- dsa40applicationhelper/backend/tests/api/test_vlopse.py
+++ dsa40applicationhelper/backend/tests/api/test_vlopse.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 
+from app.core.config import ApplicationModality, PlatformInformation
 from app.main import app
 
 client = TestClient(app)
@@ -12,12 +13,23 @@ def test_get_vlopses():
     assert isinstance(response.json(), list)
 
 
+info = PlatformInformation(
+    name="testing",
+    account_required=True,
+    application_link="localhost",
+    modality=ApplicationModality.form,
+)
+info = info.model_dump()
+
+
 def test_post_vlopse():
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 200
     response = client.get("/api/vlopse")
     assert response.status_code == 200
-    assert any(v == "testing" for v in response.json()), "Vlopse should be created"
+    assert any(v["id"] == "testing" for v in response.json()), (
+        "Vlopse should be created"
+    )
     # Cleanup
     response = client.delete("/api/vlopse/testing")
     assert response.status_code == 200
@@ -29,17 +41,18 @@ def test_post_invalid_vlopse():
 
 
 def test_post_existant():
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 200
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 409
     # Cleanup
     response = client.delete("/api/vlopse/testing")
     assert response.status_code == 200
 
 
+# FIXME
 def test_put():
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 200
 
     response = client.put("/api/vlopse/testing", json={"new_name": "new_testing"})
@@ -55,9 +68,9 @@ def test_put_non_existant():
 
 
 def test_put_new_already_exists():
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 200
-    response = client.post("/api/vlopse", json={"name": "new_testing"})
+    response = client.post("/api/vlopse", json={"id": "new_testing", "info": info})
     assert response.status_code == 200
     response = client.put("/api/vlopse/testing", json={"new_name": "new_testing"})
     assert response.status_code == 409
@@ -79,7 +92,7 @@ def test_put_no_argument():
 
 
 def test_delete():
-    response = client.post("/api/vlopse", json={"name": "testing"})
+    response = client.post("/api/vlopse", json={"id": "testing", "info": info})
     assert response.status_code == 200
     response = client.delete("/api/vlopse/testing")
     assert response.status_code == 200
